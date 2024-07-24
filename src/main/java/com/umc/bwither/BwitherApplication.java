2c0c47a7b41f0df5b6c4bcff87ad01cd814747c4
twosky0202 <leehaneul0202@naver.com>
2024-07-24 22:17:04 +0900

    🎉Init : 프로젝트 초기 세팅

---
+++ src/main/java/com/umc/bwither/BwitherApplication.java
@@ -0,0 +1,13 @@
+package com.umc.bwither;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BwitherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BwitherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umc/bwither/BwitherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.bwither;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BwitherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
