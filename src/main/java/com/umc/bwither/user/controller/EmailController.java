e9354032831f14587e2b847c72a79d7b7443aaa1
20210699 <20210699@duksung.ac.kr>
2024-08-11 06:17:28 +0900

    ✨ Feat: Add email verify
    이메일 인증 구현

--- src/main/java/com/umc/bwither/user/controller/EmailController.java
+++ src/main/java/com/umc/bwither/user/controller/EmailController.java
@@ -11,15 +11,29 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.hibernate.query.sqm.tree.SqmNode.log;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/email")
 public class EmailController {
     private final EmailServiceImpl emailService;
 
+    @PostMapping("/send_test")
+    public ResponseEntity<?> emailSendForTest(@RequestBody EmailDTO emailDTO){
+        String code = emailService.sendEmail(emailDTO);
+        return ResponseEntity.ok(ApiResponse.of(SuccessStatus.SUCCESS_EMAIL_SENT, code ));
+    }
+
     @PostMapping("/send")
     public ResponseEntity<?> emailSend(@RequestBody EmailDTO emailDTO){
-        String code = emailService.sendMail(emailDTO);
-        return ResponseEntity.ok(ApiResponse.of(SuccessStatus.SUCCESS_EMAIL_SENT, "이메일 인증 코드 : " + code ));
+        emailService.sendEmail(emailDTO);
+        return ResponseEntity.ok(ApiResponse.of(SuccessStatus.SUCCESS_EMAIL_SENT, null ));
+    }
+
+    @PostMapping("/verify")
+    public String verify(@RequestBody EmailDTO emailDTO) {
+        boolean isVerify = emailService.verifyEmailCode(emailDTO);
+        return isVerify ? "인증이 완료되었습니다." : "인증 실패하셨습니다.";
     }
 }
