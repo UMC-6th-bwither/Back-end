c1178feabfb8f119cd1c748b93c0d737c1dd2ddb
20210699 <20210699@duksung.ac.kr>
2024-08-15 20:42:54 +0900

    ♻️ Refactor: PostRequestDTO 수정

--- src/main/java/com/umc/bwither/post/dto/PostRequestDTO.java
+++ src/main/java/com/umc/bwither/post/dto/PostRequestDTO.java
@@ -3,7 +3,7 @@ package com.umc.bwither.post.dto;
 import com.umc.bwither.post.entity.enums.Category;
 import com.umc.bwither.post.entity.enums.PetType;
 import com.umc.bwither.user.entity.User;
-import lombok.Data;
+import lombok.*;
 
 import java.util.List;
 @Data
@@ -17,4 +17,53 @@ public class PostRequestDTO {
     private Category category;
     private List<BlockDTO> blocks;
 
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class GetTipDTO {
+
+        private Long userId;
+        private PetType petType;
+        private String title;
+        private Category category;
+        private List<BlockDTO> blocks;
+
+        public static PostRequestDTO.GetTipDTO getTipDTO(PostRequestDTO.GetTipDTO tipsDTO) {
+            return GetTipDTO.builder()
+                    .userId(tipsDTO.userId)
+                    .petType(tipsDTO.petType)
+                    .title(tipsDTO.title)
+                    .category(Category.TIPS)
+                    .blocks(tipsDTO.blocks)
+                    .build();
+        }
+
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class GetReviewDTO {
+
+        private Long breederId;
+        private Long userId;
+        private PetType petType;
+        private Integer rating;
+        private Category category;
+        private List<BlockDTO> blocks;
+
+        public static PostRequestDTO.GetReviewDTO getReviewDTO(PostRequestDTO.GetReviewDTO reviewDTO) {
+            return GetReviewDTO.builder()
+                    .breederId(reviewDTO.breederId)
+                    .userId(reviewDTO.userId)
+                    .petType(reviewDTO.petType)
+                    .category(Category.BREEDER_REVIEWS)
+                    .rating(reviewDTO.rating)
+                    .blocks(reviewDTO.blocks)
+                    .build();
+        }
+
+    }
 }
--- src/main/java/com/umc/bwither/post/dto/PostResponseDTO.java
+++ src/main/java/com/umc/bwither/post/dto/PostResponseDTO.java
@@ -2,14 +2,8 @@ package com.umc.bwither.post.dto;
 
 import com.umc.bwither.post.entity.enums.Category;
 import com.umc.bwither.post.entity.enums.PetType;
-import io.swagger.models.auth.In;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
+import lombok.*;
 import java.time.LocalDateTime;
-import java.util.Calendar;
 import java.util.List;
 
 @Data
@@ -26,4 +20,6 @@ public class PostResponseDTO {
     private String author; // 작성자 필드 추가
     private List<BlockDTO> blocks;
     private LocalDateTime createdAt;
+
+
 }
