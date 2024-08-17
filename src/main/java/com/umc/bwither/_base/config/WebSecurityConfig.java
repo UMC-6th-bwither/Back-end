24106308a10ee86312ec0ac7845c3b05c0fa3702
twosky0202 <leehaneul0202@naver.com>
2024-08-17 11:00:31 +0900

    🐛 Fix: 수정

--- src/main/java/com/umc/bwither/_base/config/WebSecurityConfig.java
+++ src/main/java/com/umc/bwither/_base/config/WebSecurityConfig.java
@@ -31,7 +31,7 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/","/breeder/join", "/login", "/user/join", "/breeder/{breederId}/member/{memberId}", "/user/{memberId}/view-breeders").permitAll()
 
                                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll() // Swagger 경로 허용
-                                .requestMatchers("/**").permitAll()
+                                .requestMatchers("/**", "/health").permitAll()
                                 .anyRequest().authenticated());
 
         http.addFilterAfter(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
